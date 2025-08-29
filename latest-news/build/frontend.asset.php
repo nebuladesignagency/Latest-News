6ed53737ed174e5fb8f92d7a2ebb4cef8117434c
nebuladesignagency <131961857+nebuladesignagency@users.noreply.github.com>
2025-08-29 14:20:00 +0100

    Updated

---
+++ latest-news/build/frontend.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-api-fetch'), 'version' => 'a1acd2759b81155214f0');
---
+++ latest-news/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element'), 'version' => '26a0b45238c436bd1b21');
